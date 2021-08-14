437f03e29a30295e33af2570a881278eeb8a027a
Maksim Baranov <oooinfo@mail.ru>
2021-08-14 16:02:40 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+   n = n.toString()
+   let i = n.length -1;
+   let result = '';
+   
+   while(i >= 0){
+      if(n[i] === '-') {
+        result = `${result}`;
+      } else {
+        result = `${result}${n[i]}`;
+      }
+      i = i - 1;
+   };
+   return result;
 }
